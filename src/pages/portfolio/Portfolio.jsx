4b3d5cd11d67e520e1e5f888ac59daaf51de9861
angel <abraham_will@hotmail.com>
2024-11-18 13:23:00 -0600

    Se pone leyenda de que el portafolio esta en desarrollo

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -5,6 +5,7 @@ import { Images } from './Images'
 export const Portfolio = () => {
     return (
         <section className="container mt-5">
+            En desarrollo...
             {/* <article className=" section-border  p-1 shadow text-center">
                 <h3 className="p-2 section-headings">Portfolio</h3>
                 <Images />
