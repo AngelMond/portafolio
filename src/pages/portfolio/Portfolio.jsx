596e645041a671bf81b19dd3228c2f1c0bd421dc
angel <abraham_will@hotmail.com>
2024-11-18 13:28:04 -0600

    Se modifica interfaz del portafolio

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -5,11 +5,12 @@ import { Images } from './Images'
 export const Portfolio = () => {
     return (
         <section className="container mt-5">
-            En desarrollo...
-            {/* <article className=" section-border  p-1 shadow text-center">
+            <article className=" section-border  p-1 shadow text-center">
                 <h3 className="p-2 section-headings">Portfolio</h3>
-                <Images />
-            </article> */}
+                {/* <Images /> */}
+
+                <p>En desarrollo...</p>
+            </article>
         </section>
     );
 };
